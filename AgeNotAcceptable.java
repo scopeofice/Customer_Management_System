1dc93029db7607c5f14e00659c2ac6699208c23b
Shubham Ghodkhande <130497256+scopeofice@users.noreply.github.com>
2023-05-03 01:22:48 +0530

    Exception

---
+++ AgeNotAcceptable.java
@@ -0,0 +1,9 @@
+package Exceptions;
+
+@SuppressWarnings("serial")
+public class AgeNotAcceptable extends Exception {
+    public AgeNotAcceptable(String message) {
+        super(message);
+    }
+}
+
