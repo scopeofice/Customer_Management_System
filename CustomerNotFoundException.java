5fba26ac8cedbbbdbcd127e0c9c4a83d0939e7d0
Shubham Ghodkhande <130497256+scopeofice@users.noreply.github.com>
2023-05-03 01:23:27 +0530

    Exception

---
+++ CustomerNotFoundException.java
@@ -0,0 +1,9 @@
+package Exceptions;
+
+@SuppressWarnings("serial")
+public class CustomerNotFoundException extends Exception {
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
+
