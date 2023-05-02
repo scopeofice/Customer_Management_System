ef5b5ac3d4dc3302b607cdd85c3b46ce3bd8df61
Shubham Ghodkhande <130497256+scopeofice@users.noreply.github.com>
2023-05-03 01:24:45 +0530

    Exception

---
+++ InvalidPlanException.java
@@ -0,0 +1,9 @@
+package Exceptions;
+
+@SuppressWarnings("serial")
+public class InvalidPlanException extends Exception {
+    public InvalidPlanException(String message) {
+        super(message);
+    }
+}
+
