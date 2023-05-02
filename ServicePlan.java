6482aba05a5770754e80102e682c54159718ecbd
Shubham Ghodkhande <130497256+scopeofice@users.noreply.github.com>
2023-05-03 01:15:05 +0530

    enum class

---
+++ ServicePlan.java
@@ -0,0 +1,27 @@
+package Customer_Management_System;
+
+public enum ServicePlan {
+    SILVER(1000),GOLD(2000),DIAMOND(5000),PLATINUM(10000);
+
+    private  double registrationAmount;
+
+    private ServicePlan(double registrationAmount) {
+        this.registrationAmount = registrationAmount;
+    }
+
+    public double getRegistrationAmount() {
+        return registrationAmount;
+    }
+    public double setRegistrationAmount() {
+    	return registrationAmount;
+    }
+   
+    
+
+	@Override
+	public String toString() {
+		return name()+" Plan charges: "+registrationAmount;
+	}
+    
+}
+
